3e5ddfc94e07feaa203bc71281f6e2ad1a1978f9
Tricia <tricianghiem@gmail.com>
2022-06-12 14:48:06 -0500

    rendering city, name, & date

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,59 @@
 //global variables
-var currentCity = ""
-var apiKey = "64300598ff5e5d0e4355814a64430ed9"
+var currentCity = "";
+var apiKey = "64300598ff5e5d0e4355814a64430ed9";
+var history = {};
+var searchButtonEl = document.getElementById("submit-button");
+var searchTextEl = document.getElementById("search");
+var cityNameEl = document.getElementById("city-name-date");
+
+
+// function to add api data to local storage
+function saveHistory(city, data) {
+    history[city] = data;
+    var historyString = JSON.stringify(history);
+    localStorage.setItem("weatherHistory", JSON.stringify(historyString));
+}
+
+// function to load the history from local storage - runs on page load
+function loadHistory() {
+    var history_string = localStorage.getItem("weatherHistory");
+    history = JSON.parse(history_string);
+}
 
 // function to retrieve the weather in the selected city
 function getWeatherInCity(city) {
     // build the api query string
-    var queryString = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    var queryString = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
     fetch(queryString)
     .then((response) => response.json())
     .then((response) => {
-        console.log(response)
+        // update the weather history in local storage
+        saveHistory(city, response);
+        //get image from site
+        console.log(response);
+        setActiveWeather(city, response);
+
     })
 }
 
-getWeatherInCity("san diego")
+function setActiveWeather(city, response) {
+    var currentTime = response.dt
+    var offset = response.timezone / 60 / 60;
+    var momentTime = moment.unix(currentTime).utc().utcOffset(offset);
+    let weatherIcon = `https://openweathermap.org/img/w/${response.weather[0].icon}.png`;
+    headerInnerHtml = `
+    <h3>
+        ${response.name}: ${momentTime.format("(MM/DD/YYYY)")}
+        <img src=${weatherIcon}>
+    </h3>
+    `
+    cityNameEl.innerHTML = headerInnerHtml;
+}
+
+
+searchButtonEl.addEventListener("click", function() {
+    var city = searchTextEl.value;
+    getWeatherInCity(city);
+})
+
+
